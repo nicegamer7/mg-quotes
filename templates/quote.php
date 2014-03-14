ade383825fbf83d326bca9def6980186f6747ce2
mgiulio <giulio.mainardi@gmail.com>
2014-03-14 17:08:22 +0100

    Wrap the quote text in a <p>

--- templates/quote.php
+++ templates/quote.php
@@ -1,7 +1,7 @@
 <?php global $mg_qt; ?>
 
 <blockquote class="mg_qt_quote">
-	<?php echo $mg_qt['quote']; ?>
+	<p><?php echo $mg_qt['quote']; ?></p>
 	<?php if (!empty($mg_qt['author'])): ?>
 		<footer>
 			<cite class="author"><?php echo $mg_qt['author']; ?></cite>
