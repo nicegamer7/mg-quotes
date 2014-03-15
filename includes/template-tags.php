a235f5cac1c2100fb0f990901908da91d9b73c53
mgiulio <giulio.mainardi@gmail.com>
2014-03-15 10:15:39 +0100

    Template tags fixes

--- includes/template-tags.php
+++ includes/template-tags.php
@@ -1,27 +1,5 @@
 <?php
 
-function mg_qt_quote($id) {
-	global $mg_qt_template_loader, $mg_qt;
-	
-	$mg_qt = array();
-	
-	$q = new WP_Query(array(
-		'post_type' => 'mg_qt_quote',
-		'p' => $id
-	));
-	
-	if (!$q->have_posts())
-		return;
-		
-	$q->the_post();
-	$mg_qt['quote'] = get_the_content();
-	$mg_qt['author'] = get_post_meta($id, 'mg_qt_author', true);
-	
-	wp_reset_postdata();
-	
-	$mg_qt_template_loader->get_template_part('quote');
-}
-
 /*
  * Pick one random quote
  */
@@ -45,12 +23,44 @@ function mg_qt_get_random_quote() {
 	
 	wp_reset_postdata();
 	
+	ob_start();
 	$mg_qt_template_loader->get_template_part('quote');
+	$html = ob_get_clean();
+	
+	return $html;
 }
 
 function mg_qt_random_quote() {
 	echo mg_qt_get_random_quote();
 }
-			
-//mg_qt_random_category(/* cat slug or/and name? */);
-//mg_qt_random_author(/* the author string */);
+
+/*
+ * Get a quote by its ID.
+ */
+function mg_qt_get_quote($id) {
+	global $mg_qt_template_loader, $mg_qt;
+	
+	// check $id
+	
+	$mg_qt = array();
+	
+	$q = new WP_Query(array(
+		'post_type' => 'mg_qt_quote',
+		'p' => $id
+	));
+	
+	if (!$q->have_posts())
+		return;
+		
+	$q->the_post();
+	$mg_qt['quote'] = get_the_content();
+	$mg_qt['author'] = get_post_meta($id, 'mg_qt_author', true);
+	
+	wp_reset_postdata();
+	
+	$mg_qt_template_loader->get_template_part('quote');
+}
+
+function mg_qt_quote($id) {
+	echo mg_qt_get_quote($id);
+}
