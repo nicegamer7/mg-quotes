4a4aa2b8c4e96c2442e012339b8805cc5a22bae8
mgiulio <giulio.mainardi@gmail.com>
2014-03-19 10:18:21 +0100

    Widgets: random quote

--- includes/widgets.php
+++ includes/widgets.php
@@ -1,49 +1,59 @@
 <?php
-
 add_action('widgets_init', 'mg_qt_register_widgets');
 
 class mg_qt_Random_Quote extends WP_Widget {
 
 	public function __construct() {
 		parent::__construct(
-			'mg_qt_widget_random_quote', // Base ID
-			__('mg qt Random Quote', 'mg_qt'), // Name
-			array('description' => __( 'Pick a random quotes', 'mg_qt' ), ) // Args
+			'mg_qt_widget_random_quote',
+			__('mg qt Random Quote', 'mg_qt'),
+			array(
+				'description' => __('Pick a random quotes', 'mg_qt'),
+				'classname' => 'mg_qt_widget_random_quote'
+			) 
 		);
 	}
 	
 	public function widget($args, $instance) {
-		//$title = apply_filters('widget_title', $instance['title']);
+		extract($args);
+		
+		$title = apply_filters('widget_title', 
+			empty($instance['title']) ? '' : $instance['title'], 
+			$instance, $this->id_base 
+		);
 
-		echo $args['before_widget'];
-		
-		/* if (!empty($title))
-			echo $args['before_title'] . $title . $args['after_title']; */
-		echo __('Hello, World!', 'mg_qt');
-		
-		echo $args['after_widget'];
+		echo $before_widget;
+		if ($title)
+			echo $before_title . $title . $after_title;
+
+		mg_qt_random_quote();
+
+		echo $after_widget;
 	}
 	
 	public function form($instance) {
-		/* if ( isset( $instance[ 'title' ] ) ) {
-			$title = $instance[ 'title' ];
-		}
-		else {
-			$title = __( 'New title', 'text_domain' );
-		}
+		$instance = wp_parse_args((array)$instance, array('title' => ''));
+		$title = $instance['title'];
 		?>
-		<p>
-		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
-		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
-		</p>
-		<?php  */
+			<p>
+				<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?> 
+					<input 
+						id="<?php echo $this->get_field_id('title'); ?>" 
+						name="<?php echo $this->get_field_name('title'); ?>" 
+						type="text" 
+						value="<?php echo esc_attr($title); ?>" 
+						class="widefat" 
+					/>
+				</label>
+			</p>
+		<?php
 	}
 	
 	public function update($new_instance, $old_instance) {
-		/* $instance = array();
-		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-
-		return $instance; */
+		$instance = $old_instance;
+		$new_instance = wp_parse_args((array)$new_instance, array( 'title' => ''));
+		$instance['title'] = strip_tags($new_instance['title']);
+		return $instance;
 	}
 
 }
