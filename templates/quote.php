40addf0648cd792ca9c1817c43a7809ce917585d
mgiulio <giulio.mainardi@gmail.com>
2014-03-25 08:54:44 +0100

    Quote template: add a 'meta' class to footer element

--- templates/quote.php
+++ templates/quote.php
@@ -3,7 +3,7 @@
 <blockquote class="mg_qt_quote">
 	<p><?php echo $mg_qt['quote']; ?></p>
 	<?php if (!empty($mg_qt['author'])): ?>
-		<footer>
+		<footer class="meta">
 			<cite class="author"><?php echo $mg_qt['author']; ?></cite>
 		</footer>
 	<?php endif; ?>
