c9e5ca78399d6fd164078576f9efee83c5cac2b1
mgiulio <giulio.mainardi@gmail.com>
2014-03-27 09:05:24 +0100

    Widgets: handle deleted quotes in the single quote widget

--- includes/widgets.php
+++ includes/widgets.php
@@ -294,8 +294,11 @@ class mg_qt_Single_Quote extends WP_Widget {
 		$instance = wp_parse_args((array)$instance, $this->factory_settings);
 		
 		$title = esc_attr($instance['title']);
-		$current_quote = $instance['quote'];
 		$quotes = mg_qt_get_quotes();
+		
+		$current_quote = $instance['quote'];
+		if ($current_quote !== 0 && !isset($quotes[$current_quote]))
+			$current_quote = 0;
 		
 		$title_field_id = $this->get_field_id('title');
 		$title_field_name = $this->get_field_name('title');
@@ -306,9 +309,9 @@ class mg_qt_Single_Quote extends WP_Widget {
 			<p><label for="<?php echo $title_field_id; ?>"><?php _e('Title:', 'mg_qt'); ?></label> 
 			<input class="widefat" id="<?php echo $title_field_id; ?>" name="<?php echo $title_field_name; ?>" type="text" value="<?php echo $title; ?>" /></p>
 			<p>
-				<label for="<?php echo $quote_field_id; ?>"><?php _e('Author:', 'mg_qt'); ?></label> 
+				<label for="<?php echo $quote_field_id; ?>"><?php _e('Quote:', 'mg_qt'); ?></label> 
 				<select id="<?php echo $quote_field_id?>" name="<?php echo $quote_field_name; ?>" class="widefat">
-					<option value="0">Select a quote</option>
+					<option value="0"<?php selected($current_quote, 0); ?>>Select a quote</option>
 					<?php foreach ($quotes as $id => $title): ?>
 						<?php
 						if (strlen($title) > 30)
