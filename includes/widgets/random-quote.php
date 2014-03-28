cf92238c86c81257078edb3a229fc90590671b9e
mgiulio <giulio.mainardi@gmail.com>
2014-03-28 14:18:19 +0100

    Widgets: merge the random-quote-widget-from-category in the random-quote one

--- includes/widgets/random-quote.php
+++ includes/widgets/random-quote.php
@@ -3,7 +3,8 @@
 class mg_qt_Random_Quote extends WP_Widget {
 
 	private $factory_settings = array(
-		'title' => ''
+		'title' => '',
+		'category' => 0
 	);
 
 	public function __construct() {
@@ -11,16 +12,67 @@ class mg_qt_Random_Quote extends WP_Widget {
 			'mg_qt_widget_random_quote',
 			__('Random Quote', 'mg_qt'),
 			array(
-				'description' => __('Pick a random quotes', 'mg_qt'),
+				'description' => __('Pick a random quotes. Supports categories', 'mg_qt'),
 				'classname' => 'mg_qt_widget_random_quote'
 			) 
 		);
 	}
 	
+	public function form($instance) {
+		$instance = wp_parse_args((array)$instance, $this->factory_settings);
+		
+		$title = esc_attr($instance['title']);
+		
+		$category = $instance['category'];
+		if ($category !== 0 && term_exists($category, 'mg_qt_category') === 0)
+			$category = 0;
+			
+		?>
+			<p>
+				<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'mg_qt'); ?> 
+					<input 
+						id="<?php echo $this->get_field_id('title'); ?>" 
+						name="<?php echo $this->get_field_name('title'); ?>" 
+						type="text" 
+						value="<?php echo $title; ?>" 
+						class="widefat" 
+					/>
+				</label>
+			</p>
+			<p>
+				<label for="<?php echo $this->get_field_id('category'); ?>"><?php _e('Category:', 'mg_qt'); ?></label> 
+				<?php
+					wp_dropdown_categories(array(
+						'taxonomy' => 'mg_qt_category',
+						'name' => $this->get_field_name('category'),
+						'selected' => $category,
+						'show_option_all' => __('View all categories', 'mg_qt'),
+						'hierarchical' => 1,
+						'orderby' => 'name',
+						'class' => 'widefat'
+					));
+				?>
+			</p>
+		<?php
+	}
+	
+	public function update($new_instance, $old_instance) {
+		$instance = $old_instance;
+		
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['category'] = absint($new_instance['category']);
+		
+		return $instance;
+	}
+	
 	public function widget($args, $instance) {
 		$instance = wp_parse_args((array)$instance, $this->factory_settings);
 		
-		$quote_markup = mg_qt_get_random_quote();
+		$quote_markup = $instance['category'] === 0 ? 
+			mg_qt_get_random_quote() : 
+			mg_qt_get_random_quote_from_category_id($instance['category'])
+		;
+		
 		if ($quote_markup === '')
 			return;
 		
@@ -36,32 +88,6 @@ class mg_qt_Random_Quote extends WP_Widget {
 			echo $before_title . $title . $after_title;
 		echo $quote_markup;
 		echo $after_widget;
-	}
-	
-	public function form($instance) {
-		$instance = wp_parse_args((array)$instance, $this->factory_settings);
-		$title = esc_attr($instance['title']);
-		?>
-			<p>
-				<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'mg_qt'); ?> 
-					<input 
-						id="<?php echo $this->get_field_id('title'); ?>" 
-						name="<?php echo $this->get_field_name('title'); ?>" 
-						type="text" 
-						value="<?php echo esc_attr($title); ?>" 
-						class="widefat" 
-					/>
-				</label>
-			</p>
-		<?php
-	}
-	
-	public function update($new_instance, $old_instance) {
-		$instance = $old_instance;
-		
-		$instance['title'] = strip_tags($new_instance['title']);
-		
-		return $instance;
 	}
 
 }
