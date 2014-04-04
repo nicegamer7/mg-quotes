0e999bc85cec0ae4b39aaab65d72ddaab3b84a32
mgiulio <giulio.mainardi@gmail.com>
2014-04-04 09:44:30 +0200

    List table: Add author dropdown

--- includes/admin/list-table.php
+++ includes/admin/list-table.php
@@ -3,7 +3,7 @@
 add_filter('manage_mg_qt_quote_posts_columns', 'mg_qt_custom_columns');
 //add_filter('manage_posts_custom_column', 'mg_qt_custom_columns_data', 10, 2);
 add_filter('months_dropdown_results', 'mg_qt_remove_months_dropdown', 10, 2);
-add_action('restrict_manage_posts', 'mg_qt_category_dropdown');
+add_action('restrict_manage_posts', 'mg_qt_taxonomy_dropdowns');
 
 function mg_qt_custom_columns($columns) {
 	$columns = array(
@@ -32,17 +32,31 @@ function mg_qt_remove_months_dropdown($months, $post_type) {
 	return $post_type === 'mg_qt_quote' ? array() : $months;
 }
 
-function mg_qt_category_dropdown() {
+function mg_qt_taxonomy_dropdowns() {
 	global $typenow;
     
 	if ($typenow !== 'mg_qt_quote')
 		return;
 		
-	$selected = get_query_var('mg_qt_category');
+	require_once MG_QT_INCLUDES . 'admin/tax-dropdown.php';
+	
+	$selected = get_query_var('mg_qt_author');
 	if ($selected === '')
 		$selected = 0;
 		
-	require_once MG_QT_INCLUDES . 'admin/tax-dropdown.php';
+	wp_dropdown_categories(array(
+		'taxonomy' => 'mg_qt_author',
+		'name' => 'mg_qt_author',
+		'selected' => $selected,
+		'walker' => new mg_qt_Walker_TaxonomyDropdown(),
+		'show_option_all' => __('View all authors', 'mg_qt'),
+		'hierarchical' => 0,
+		'orderby' => 'name'
+	));
+	
+	$selected = get_query_var('mg_qt_category');
+	if ($selected === '')
+		$selected = 0;
 		
 	wp_dropdown_categories(array(
 		'taxonomy' => 'mg_qt_category',
