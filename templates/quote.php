51e517f155c187b75e05c9a2e0576f8ca1d5aa84
mgiulio <giulio.mainardi@gmail.com>
2014-04-12 10:06:57 +0200

    Rename 'quote' key to 'content

--- templates/quote.php
+++ templates/quote.php
@@ -1,7 +1,7 @@
 <?php global $quote; ?>
 
 <blockquote class="mg_qt_quote">
-	<?php echo $quote['quote']; ?>
+	<?php echo $quote['content']; ?>
 	<?php if (!empty($quote['author'])): ?>
 		<footer class="meta">
 			<cite class="author"><?php echo $quote['author']; ?></cite>
